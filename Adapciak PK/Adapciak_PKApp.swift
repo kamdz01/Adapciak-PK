cef91b4210a62f5458f129998fe2b1458b833c1e
Kamil Dziedzic <kamdz01@gmail.com>
2023-07-06 19:26:52 +0200

    Initial Commit

---
+++ Adapciak PK/Adapciak_PKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adapciak_PKApp.swift
+//  Adapciak PK
+//
+//  Created by Kamil Dziedzic on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Adapciak_PKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
